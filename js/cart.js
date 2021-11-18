6576fb82e4315f31bd4ab447a2364e081a7c89ac
Katarina Kyrylenko <katarina.kyrylenko@gmail.com>
2021-11-18 20:08:00 +0200

    add everything

---
+++ js/cart.js
@@ -0,0 +1,104 @@
+const buttonCart = document.querySelector(".button-cart");
+const buttonSend = document.querySelector(".button-send");
+const modalCart = document.querySelector(".modal-cart");
+const close = modalCart.querySelector(".close");
+const body = modalCart.querySelector(".modal-body");
+console.log(buttonSend);
+
+const resetCart = () => {
+  body.innerHTML = "";
+  localStorage.removeItem("cart");
+  modalCart.classList.remove("is-open");
+};
+
+const incrementCount = (id) => {
+  const cartArray = JSON.parse(localStorage.getItem("cart"));
+
+  cartArray.map((item) => {
+    if (item.id === id) {
+      item.count++;
+    }
+    return item;
+  });
+  localStorage.setItem("cart", JSON.stringify(cartArray));
+  renderItems(cartArray);
+};
+
+const decrementCount = (id) => {
+  const cartArray = JSON.parse(localStorage.getItem("cart"));
+
+  cartArray.map((item) => {
+    if (item.id === id) {
+      item.count = item.count > 0 ? item.count - 1 : 0;
+    }
+    return item;
+  });
+  localStorage.setItem("cart", JSON.stringify(cartArray));
+  renderItems(cartArray);
+};
+
+const renderItems = (data) => {
+  body.innerHTML = " ";
+  data.forEach(({ name, price, id, count }) => {
+    const cartElem = document.createElement("div");
+    cartElem.classList.add("food-row");
+
+    cartElem.innerHTML = `
+    <span class="food-name">${name}</span>
+            <strong class="food-price">${price} ₽</strong>
+            <div class="food-counter">
+              <button class="counter-button btn-dec" data-index="${id}">-</button>
+              <span class="counter">${count}</span>
+              <button class="counter-button btn-inc" data-index="${id}">+</button>
+            </div>
+    `;
+    // cartElem.querySelector(".btn-dec").addEventListener("click", () => {
+    //   decrementCount(id);
+    // });
+    // cartElem.querySelector(".btn-inc").addEventListener("click", () => {
+    //   incrementCount(id);
+    // });
+
+    body.append(cartElem);
+  });
+};
+
+body.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("btn-inc")) {
+    incrementCount(e.target.dataset.index);
+  } else if (e.target.classList.contains("btn-dec")) {
+    decrementCount(e.target.dataset.index);
+  }
+});
+
+buttonSend.addEventListener("click", () => {
+  const cartArray = localStorage.getItem("cart");
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: cartArray,
+  })
+    .then((response) => {
+      if (response) {
+        resetCart();
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+
+  console.log(cartArray);
+});
+
+buttonCart.addEventListener("click", () => {
+  if (localStorage.getItem("cart")) {
+    renderItems(JSON.parse(localStorage.getItem("cart")));
+  }
+
+  modalCart.classList.add("is-open");
+});
+
+close.addEventListener("click", () => {
+  modalCart.classList.remove("is-open");
+});
